58e089612c73d7068b2744152da6a4b7ba5e72b9
Kajal <kajalpatil158@gmail.com>
2021-04-08 18:35:20 +0530

    [Kajal PAtil]Added: Create Contact

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,9 +1,28 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
 		System.out.println("Wellcome To Address Book Program... ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter FirstName :");
+		String firstName = sc.next();
+		System.out.println("Enter LastName :");
+		String lastName = sc.next();
+		System.out.println("Enter Address :");
+		String address = sc.next();
+		System.out.println("Enter City :");
+		String city = sc.next();
+		System.out.println("Enter State :");
+		String state = sc.next();
+		System.out.println("Enter Zip :");
+		String zip = sc.next();
+		System.out.println("Enter Phone Number :");
+		String phoneNumber = sc.next();
+		System.out.println("Enter Email ID :");
+		String email = sc.next();
 	}
 
 }
